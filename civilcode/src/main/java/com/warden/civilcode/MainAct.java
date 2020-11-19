54d70e24887516b9c48121b73318ca0285979e20
余斌 <wardenlzr@qq.com>
2020-11-19 11:47:17 +0800

    民法典APP

---
+++ civilcode/src/main/java/com/warden/civilcode/MainAct.java
@@ -0,0 +1,51 @@
+package com.warden.civilcode;
+
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+        ProgressBar pb = findViewById(R.id.pb);
+        pb.setVisibility(View.VISIBLE);
+        WebView webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        //只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。本地没有缓存时才从网络上获取。
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 设置可以访问文件
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.loadUrl("http://app.uhetong.com:9988/civil/");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 加载完成
+                pb.postDelayed(() -> {
+                    pb.setVisibility(View.GONE);
+                }, 500);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                // 加载开始
+            }
+
+        });
+    }
+}
